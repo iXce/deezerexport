6c9ea0903a659cbba29edfa8250cd67f39d38def
Guillaume Seguin <guillaume@segu.in>
2013-08-09 11:27:39 +0200

    Add json export

--- deezerexport.py
+++ deezerexport.py
@@ -47,13 +47,29 @@ def get_playlist(playlistid):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description = 'Deezer export script')
     parser.add_argument('uid', type = int, help = "User identifier")
+    parser.add_argument('-j', '--json', '--export', dest = "export",
+                        help = "Export playlists in a simple json format"
+                               "to the specified file")
     args = parser.parse_args()
     userdata = get_user_info(args.uid)
     print("Playlists for user %s" % userdata["name"])
     playlists = get_user_playlists(args.uid)
+    playlists_data = []
     for row in playlists["data"]:
         playlistid = row["id"]
         playlist = get_playlist(playlistid)
+        playlists_data.append(playlist)
         print("  Playlist %s" % playlist["title"])
         for track in playlist["tracks"]["data"]:
             print("    %s -- by %s" % (track["title"], track["artist"]["name"]))
+    if args.export is not None:
+        data = {"uid": args.uid, "user": userdata["name"], "playlists": []}
+        for playlist in playlists_data:
+            tracks = [{"title": track["title"],
+                       "artist": track["artist"]["name"],
+                       "album": track["album"]["title"]}
+                      for track in playlist["tracks"]["data"]]
+            cur_playlist = {"title": playlist["title"], "tracks": tracks}
+            data["playlists"].append(cur_playlist)
+        with open(args.export, "w") as f:
+            json.dump(data, f)
